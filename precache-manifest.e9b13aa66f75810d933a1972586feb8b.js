1039ac8333662c9ec81c563c28b7813992413610
Arnas <1arnasdickus1@gmail.com>
2019-07-08 20:24:29 +0300

    Updates

---
+++ precache-manifest.e9b13aa66f75810d933a1972586feb8b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2e338575d32d303da14dd07570520e",
+    "url": "/Portfolio-react/index.html"
+  },
+  {
+    "revision": "68006b3d46e409a48042",
+    "url": "/Portfolio-react/static/css/2.f74a8007.chunk.css"
+  },
+  {
+    "revision": "148642c497b9e7c505b6",
+    "url": "/Portfolio-react/static/css/main.f0f1d408.chunk.css"
+  },
+  {
+    "revision": "68006b3d46e409a48042",
+    "url": "/Portfolio-react/static/js/2.5a1d3ba3.chunk.js"
+  },
+  {
+    "revision": "148642c497b9e7c505b6",
+    "url": "/Portfolio-react/static/js/main.98066f66.chunk.js"
+  },
+  {
+    "revision": "afaa1a312de30a8694a0",
+    "url": "/Portfolio-react/static/js/runtime~main.3837020e.js"
+  },
+  {
+    "revision": "32ee0e5132b3c51657c1b146f44db358",
+    "url": "/Portfolio-react/static/media/Forkify-1920.32ee0e51.jpg"
+  },
+  {
+    "revision": "06d9ba493117471ef91188fca646e3cf",
+    "url": "/Portfolio-react/static/media/Portfolio.06d9ba49.jpg"
+  },
+  {
+    "revision": "9615df5824848a20e059d7d725d7f08b",
+    "url": "/Portfolio-react/static/media/enigma-assignment-1920.9615df58.jpg"
+  },
+  {
+    "revision": "5a5ce226ed6c20658a8b236926d4eea6",
+    "url": "/Portfolio-react/static/media/friendlist-image.5a5ce226.jpg"
+  },
+  {
+    "revision": "f55e00ce624434f3218862359df41dbb",
+    "url": "/Portfolio-react/static/media/full-stack-weather-app.f55e00ce.jpg"
+  },
+  {
+    "revision": "e2833cd4002591c2ff69f9d4531ff4d3",
+    "url": "/Portfolio-react/static/media/intro-bg-1920.e2833cd4.jpg"
+  },
+  {
+    "revision": "b1485ec370488b804b8f862f2e24578d",
+    "url": "/Portfolio-react/static/media/mappypals-board-image.b1485ec3.jpg"
+  },
+  {
+    "revision": "1ed14542a9eee035385dd30c418e11df",
+    "url": "/Portfolio-react/static/media/mappypals-board-thumbnail.1ed14542.jpg"
+  },
+  {
+    "revision": "bd090fb9effda9a8e1997fe60f8d2891",
+    "url": "/Portfolio-react/static/media/open-source-project-image.bd090fb9.jpg"
+  },
+  {
+    "revision": "30de30b8333d9c7aa96d17306da78e37",
+    "url": "/Portfolio-react/static/media/progress-week-report-img.30de30b8.jpg"
+  },
+  {
+    "revision": "780eb1bb737bdbb9ce256d52ba750ca3",
+    "url": "/Portfolio-react/static/media/sign-up-img.780eb1bb.jpg"
+  },
+  {
+    "revision": "27e2a5eecbe54f5f1cd5c8f1bc544de1",
+    "url": "/Portfolio-react/static/media/sign-up-validation-img.27e2a5ee.jpg"
+  },
+  {
+    "revision": "9279b101a665985146eb7d6c44bf86bb",
+    "url": "/Portfolio-react/static/media/task-card-full-img.9279b101.jpg"
+  }
+]);

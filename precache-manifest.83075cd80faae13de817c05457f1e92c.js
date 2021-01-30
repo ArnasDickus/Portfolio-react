b8d291d8ee1d8cd66cfd5fc66721a8acfa792a84
Arnas Dickus <arnasdickus@protonmail.com>
2021-01-30 09:57:58 +0200

    Updates

---
+++ precache-manifest.83075cd80faae13de817c05457f1e92c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc430da61d50946f22adb1deb93bc38",
+    "url": "/Portfolio-react/index.html"
+  },
+  {
+    "revision": "6d16297feee951ced374",
+    "url": "/Portfolio-react/static/css/2.b7142215.chunk.css"
+  },
+  {
+    "revision": "13c80b86c86d7c3a1079",
+    "url": "/Portfolio-react/static/css/main.36c70a8d.chunk.css"
+  },
+  {
+    "revision": "6d16297feee951ced374",
+    "url": "/Portfolio-react/static/js/2.ab1a8e43.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/Portfolio-react/static/js/2.ab1a8e43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c80b86c86d7c3a1079",
+    "url": "/Portfolio-react/static/js/main.63c21fe3.chunk.js"
+  },
+  {
+    "revision": "aaae9b6b8051369ddfcb",
+    "url": "/Portfolio-react/static/js/runtime-main.5cf0d063.js"
+  },
+  {
+    "revision": "df478a4aa341a36b0bfe710e8a71e9a7",
+    "url": "/Portfolio-react/static/media/Emendis_logo.df478a4a.jpg"
+  },
+  {
+    "revision": "e4f225ffcb12f4f5201e3541d51a307d",
+    "url": "/Portfolio-react/static/media/angular-email.e4f225ff.png"
+  },
+  {
+    "revision": "7c63825c874f821e18d52b04381d60f4",
+    "url": "/Portfolio-react/static/media/breaking-bad.7c63825c.png"
+  },
+  {
+    "revision": "2b6843381828272b34bec0f393af9938",
+    "url": "/Portfolio-react/static/media/crown-clothing.2b684338.jpg"
+  },
+  {
+    "revision": "9615df5824848a20e059d7d725d7f08b",
+    "url": "/Portfolio-react/static/media/enigma-assignment-1920.9615df58.jpg"
+  },
+  {
+    "revision": "5a5ce226ed6c20658a8b236926d4eea6",
+    "url": "/Portfolio-react/static/media/friendlist-image.5a5ce226.jpg"
+  },
+  {
+    "revision": "e2833cd4002591c2ff69f9d4531ff4d3",
+    "url": "/Portfolio-react/static/media/intro-bg-1920.e2833cd4.jpg"
+  },
+  {
+    "revision": "b1485ec370488b804b8f862f2e24578d",
+    "url": "/Portfolio-react/static/media/mappypals-board-image.b1485ec3.jpg"
+  },
+  {
+    "revision": "1ed14542a9eee035385dd30c418e11df",
+    "url": "/Portfolio-react/static/media/mappypals-board-thumbnail.1ed14542.jpg"
+  },
+  {
+    "revision": "bd090fb9effda9a8e1997fe60f8d2891",
+    "url": "/Portfolio-react/static/media/open-source-project-image.bd090fb9.jpg"
+  },
+  {
+    "revision": "30de30b8333d9c7aa96d17306da78e37",
+    "url": "/Portfolio-react/static/media/progress-week-report-img.30de30b8.jpg"
+  },
+  {
+    "revision": "780eb1bb737bdbb9ce256d52ba750ca3",
+    "url": "/Portfolio-react/static/media/sign-up-img.780eb1bb.jpg"
+  },
+  {
+    "revision": "27e2a5eecbe54f5f1cd5c8f1bc544de1",
+    "url": "/Portfolio-react/static/media/sign-up-validation-img.27e2a5ee.jpg"
+  },
+  {
+    "revision": "9279b101a665985146eb7d6c44bf86bb",
+    "url": "/Portfolio-react/static/media/task-card-full-img.9279b101.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.416013927ffb8e641fe2cefac15ef34e.js"
+  "/Portfolio-react/precache-manifest.83075cd80faae13de817c05457f1e92c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
